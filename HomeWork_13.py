496b6313e098b47ff53c0fd37e11f8cd105e41da
VNMad <vadiknoname@gmail.com>
2026-02-22 13:48:54 +0100

    Homework 13 done

---
+++ HomeWork_13.py
@@ -0,0 +1,43 @@
+#____________________________________________________________________
+#1. Прогрессия увеличения
+#____________________________________________________________________
+# numbers = (3, 7, 2, 8, 5, 10, 1)
+#
+# result = []
+# max_digit = 0
+#
+# for i in numbers:
+#     if i > max_digit:
+#         result.append(i)
+#         max_digit = i
+#
+# print("Кортеж по возрастанию:", tuple(result))
+
+#____________________________________________________________________
+#2. Повторяющиеся элементы v.2.1
+#____________________________________________________________________
+# numbers = (1, 2, 3, 4, 2, 5, 3, 6, 4, 2, 9)
+# checked = []
+#
+# for num in numbers:
+#     if num not in checked and numbers.count(num) > 1:
+#         print(f"Индексы элемента {num}:", end=" ")
+#         for i in range(len(numbers)):
+#             if numbers[i] == num:
+#                 print(i, end=" ")
+#         checked.append(num)
+#         print()
+#____________________________________________________________________
+#2. Повторяющиеся элементы v.2.2
+#____________________________________________________________________
+numbers = (1, 2, 3, 4, 2, 5, 3, 6, 4, 2, 9)
+checked = []
+
+for num in numbers:
+    if num not in checked and numbers.count(num) > 1:
+        indexes = []
+        for index, item in enumerate(numbers):
+            if item == num:
+                indexes.append(index)
+        print(f"Индексы элемента {num}:", *indexes)
+        checked.append(num)
