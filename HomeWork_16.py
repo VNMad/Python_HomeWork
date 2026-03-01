ada8b8d6a892032feba68b7ee7bd2ea1058b9778
VNMad <vadiknoname@gmail.com>
2026-03-01 22:28:55 +0100

    Homework 16 done

---
+++ HomeWork_16.py
@@ -0,0 +1,29 @@
+#____________________________________________________________________
+#1. Оценки текстом
+#____________________________________________________________________
+grades = [5, 3, 4, 2, 1, 5, 3]
+print([[item, ('отлично' if item==5 else 'хорошо' if 3<=item<=4 else 'неудовлетворительно')]
+       for item in grades])
+
+#____________________________________________________________________
+#2. Правильные скобки
+#____________________________________________________________________
+#string = "({[}])"
+string = "([({}()){}])"
+stack = []
+
+for i in range(len(string)):
+    char = string[i]
+    if char in '([{':
+        stack.append(char)
+    elif char == ')' and len(stack) > 0 and stack[-1] == '(':
+        stack.pop()
+    elif char == ']' and len(stack) > 0 and stack[-1] == '[':
+        stack.pop()
+    elif char == '}' and len(stack) > 0 and stack[-1] == '{':
+        stack.pop()
+    else:
+        print("Валидны: False")
+        break
+else:
+    print("Валидны:, True")
