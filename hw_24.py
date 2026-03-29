acbba934052dea08a59fb1aa2e4005db2e3da43d
VNMad <vadiknoname@gmail.com>
2026-03-29 23:08:10 +0200

    Homework 24 done

---
+++ hw_24.py
@@ -0,0 +1,34 @@
+#____________________________________________________________________
+#1. Сумма цифр числа
+#____________________________________________________________________
+#from functools import lru_cache
+
+#@lru_cache(maxsize=None)
+def sum_digits(n: int) -> int:
+
+    if n == 0:
+        return 0
+
+    return sum_digits(n // 10) + n % 10
+
+
+num = 43197
+print(sum_digits(num))
+
+#____________________________________________________________________
+#2. Сумма вложенных чисел
+#____________________________________________________________________
+from typing import Any
+
+def sum_num(data: list[Any]) -> int:
+    summa = 0
+    for i in data:
+        if type(i) == list:
+            summa += sum_num(i)
+        else:
+            summa += i
+    return summa
+
+
+nested_numbers = [1, [2, 3], [4, [5, 6]], 7]
+print(sum_num(nested_numbers))
