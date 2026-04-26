314e13eaf34c0a6821df24b404027d7f50e96069
VNMad <vadiknoname@gmail.com>
2026-04-27 01:09:08 +0200

    Homework 31 done

--- hw_31.py
+++ hw_31.py
@@ -4,8 +4,7 @@
 import re
 
 text = "The events N 123456 happened on 15/03/2025, 01.12.2024 and 09-09-2023. Deadline 28/02/2022."
-pattern = r"\b\d{2}[./-]\d{2}[./-]\d{4}\b"
-dates = re.findall(pattern, text)
+dates = re.findall(r"\b\d{2}[./-]\d{2}[./-]\d{4}\b", text)
 
 for date in dates:
     print(date)
